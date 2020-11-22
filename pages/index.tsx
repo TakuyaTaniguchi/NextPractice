18dd7337031f65f59bd8d4f9d209f453874f925a
Takuya Taniguchi <ttaniguchi@binc.jp>
2020-11-22 22:13:55 +0900

    Fin:DynamicRouting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
 import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
+import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+import {getSortedPostsData} from '../lib/posts'
 
-export default function Home({ allPostsData }) {
+import Link from 'next/link'
+import Date from '../components/date'
+
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
@@ -15,13 +18,17 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
+          {allPostsData.map(({id, date, title}) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                {title}
+              </Link>
               <br />
               {id}
               <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                {date}
+              </small>
             </li>
           ))}
         </ul>
