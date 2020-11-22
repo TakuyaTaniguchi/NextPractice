b8ca82560311ad9c585118d4d02d4804bf739717
Takuya Taniguchi <ttaniguchi@binc.jp>
2020-11-22 17:11:41 +0900

    動的routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
 import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
-import Alert  from '../components/alert'
 import { getSortedPostsData } from '../lib/posts'
-
 
 export default function Home({ allPostsData }) {
   return (
-    <Layout home='home'>
-      <Head>…</Head>
-      <section className={utilStyles.headingMd}>…</section>
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+      </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
